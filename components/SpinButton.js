eca9b4e1bffb01761bb9892a32228b82e135f6c8
Harr-Yates <hyates1@gmail.com>
2024-03-15 21:53:54 +0100

    added new background

--- components/SpinButton.js
+++ components/SpinButton.js
@@ -8,45 +8,52 @@ export default class SpinButton {
   }
 
   createButton() {
-    const buttonSize = 60;
+    const buttonSize = 60; // You might want to adjust this for your needs
 
+    // Create the play icon
     const playIcon = new PIXI.Graphics();
-    playIcon.beginFill(0xffffff);
-    playIcon.moveTo(1, 0);
-    playIcon.lineTo(20, 10);
-    playIcon.lineTo(0, 20);
-    playIcon.lineTo(0, 0);
+    playIcon.beginFill(0xffd700); // Gold color
+    playIcon.drawPolygon([1, 0, 20, 10, 1, 20]);
     playIcon.endFill();
     playIcon.x = buttonSize / 2.7;
     playIcon.y = buttonSize / 3;
 
+    // Create the pause icon
     const pauseIcon = new PIXI.Graphics();
-    pauseIcon.beginFill(0xffffff);
+    pauseIcon.beginFill(0xffd700); // Gold color
     pauseIcon.drawRect(0, 0, 6, 20);
     pauseIcon.drawRect(14, 0, 6, 20);
     pauseIcon.x = buttonSize / 3;
     pauseIcon.y = buttonSize / 3;
 
+    // Create the spin button with a new style
     const spinButton = new PIXI.Graphics();
-    this.updateButtonStyle(spinButton, 0x333333, buttonSize);
+    this.updateButtonStyle(spinButton, 0x8b0000, buttonSize); // Deep red
 
+    // Position the button as per your layout
     spinButton.x = (this.app.screen.width - buttonSize) / 2;
     spinButton.y = this.app.screen.height - buttonSize - 20;
     spinButton.interactive = true;
     spinButton.buttonMode = true;
 
+    // Add the appropriate icon based on the spinning state
     spinButton.addChild(this.spinning ? pauseIcon : playIcon);
 
+    // Event listener for the button click
     spinButton.on("pointerdown", () => {
       this.onSpinButtonClicked(spinButton, playIcon, pauseIcon);
     });
 
+    // Add the button to the stage
     this.app.stage.addChild(spinButton);
   }
 
   updateButtonStyle(graphic, fillColor, size) {
+    // Clear the previous style
     graphic.clear();
-    graphic.lineStyle(2, 0xffffff);
+
+    // Decorative border style
+    graphic.lineStyle(3, 0xffd700); // Gold color
     graphic.beginFill(fillColor);
     graphic.drawCircle(size / 2, size / 2, size / 2);
     graphic.endFill();
